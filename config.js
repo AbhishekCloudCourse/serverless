5635fffe795f8c13558ca7e3dc0c841bdf477838
Abhishek <gopalakrishnanunni.a@northeastern.edu>
2024-03-27 17:48:10 -0400

    update

--- config.js
+++ config.js
@@ -2,9 +2,13 @@ const Pool = require("pg").Pool;
 const dotenv = require("dotenv");
 dotenv.config();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
-});
+  const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD
+  });
+
 
 pool.on("connect", () => {
   console.log("connected to the db");
