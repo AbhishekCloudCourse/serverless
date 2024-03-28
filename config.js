09c396a71b6e222ec8b71ae4ccf0401e4fb72b7b
Abhishek <gopalakrishnanunni.a@northeastern.edu>
2024-03-28 12:39:04 -0400

    updates

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
   const pool = new Pool({
-    user: process.env.DB_USER,
+    user: process.env.DB_USERNAME,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD
